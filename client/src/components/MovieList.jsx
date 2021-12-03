bff97b00c8ca5be0193ec97b3abdb6aa10ceae6e
Kavita Poduri <kavi.pdri@gmail.com>
2021-12-02 16:59:08 -0800

    started watch toggle work - minimal progress

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const MovieList = (props) => (
-  props.movies.map( (movie, index) =>
-    <div className="movie-entry" key = {index}>{movie.title}</div>
+  props.movies.map( (movie) =>
+    <div className="movie-entry" key = {movie.title}>
+      {movie.title}      <button>Watched</button>
+    </div>
   )
 );
 
